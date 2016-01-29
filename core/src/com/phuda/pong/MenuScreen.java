2561d75f6b369a18754dd8326f6b01f9f69f6543
honix <ted888@ya.ru>
2016-01-29 14:25:00 +0300

    Menu visual improvement

--- core/src/com/phuda/pong/MenuScreen.java
+++ core/src/com/phuda/pong/MenuScreen.java
@@ -3,6 +3,7 @@ package com.phuda.pong;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.phuda.pong.UI.Button;
 import com.phuda.pong.UI.Slider;
@@ -11,20 +12,25 @@ public class MenuScreen extends PongScreen
 {
 	//Menu menu;
 	//MenuRenderer menu;
+    final int w = Gdx.graphics.getWidth(), h = Gdx.graphics.getHeight();
 	SpriteBatch batch;
 	Button start_pvp_button, start_pvc_button, other2_button;
     Slider balls_slider, ai_mode_slider;
+    Texture backGround;
+    float backGroundRotation;
 
 	MenuScreen(Game game)
 	{
 		super(game);
-        int y = (int) (Gdx.graphics.getHeight() / 3 * 1.5);
+        int y = (int) (Gdx.graphics.getHeight() / 1.7);
         int x = Gdx.graphics.getWidth() / 4;
 
         String images_path = "images_hi/";
 		start_pvp_button = new Button(x,   y,      images_path + "pvp.png");
 		start_pvc_button = new Button(x*2, y + y/3,images_path + "pvc.png");
 		other2_button =    new Button(x*3, y,      images_path + "undef.png");
+        backGround = new Texture(Gdx.files.internal(images_path + "background.png"));
+        backGround.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 
         balls_slider = new Slider(x*2, y - y/2, 1, 12, 2, "Balls count: ");
         ai_mode_slider = new Slider(x*2, y - (int)(y/1.2), 1, 3, 2, "AI strength: ");
@@ -38,6 +44,7 @@ public class MenuScreen extends PongScreen
 
 	public void render(float delta)
 	{
+        backGroundRotation += delta * 2;
 
 		if (start_pvp_button.isPressed()){
 			game.setScreen(new GameScreen(game, balls_slider.value, 0));
@@ -52,10 +59,15 @@ public class MenuScreen extends PongScreen
         balls_slider.isPressed();
         ai_mode_slider.isPressed();
 
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		Gdx.gl.glClearColor(.25f, .25f, .25f, 1f);
-		
+        Gdx.gl.glClearColor(.15f, .05f, .05f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
+        batch.draw(backGround, w/2 - backGround.getWidth()/2, h/2 - backGround.getHeight()/2,
+                backGround.getWidth()/2, backGround.getHeight()/2,
+                backGround.getWidth(), backGround.getHeight(),
+                1, 1, backGroundRotation, 0, 0, backGround.getWidth(), backGround.getHeight(),
+                false, false);
 		start_pvp_button.draw(batch);
 		start_pvc_button.draw(batch);
 		other2_button.draw(batch);
