0c8103ba6a2f451080fada2aeac30a835a22d1fe
SkyReMore <skyremore@mail.ru>
2016-02-05 13:10:45 +0300

    Sliders improvements
    Class Slider
    Potential bug with number range fixed. Also touch zone not going out of scrolling line's bounds now.
    Fixed slider's pointer positions added.
    Slider's size now depends on screen size (buttons are next)

--- core/src/com/phuda/pong/UI/Slider.java
+++ core/src/com/phuda/pong/UI/Slider.java
@@ -12,33 +12,33 @@ import com.badlogic.gdx.math.Rectangle;
 public class Slider {
 
     public int value;
-    private int ux, sx, x, y, min, max, offset;
+    private int ux, sx, x, y, min, max, screenWidth, screenHeight;
+    private float offset, space;
     private String label;
-    private Rectangle rect;
+    private Rectangle bounds;
     private Texture base, unit;
     private BitmapFont font;
 
     public Slider(int _x, int _y, int _min, int _max, int defValue, String _label){
-
+        screenWidth = Gdx.graphics.getWidth();
+        screenHeight = Gdx.graphics.getHeight();
         String images_path = "images_hi/";
         base = new Texture(Gdx.files.internal(images_path + "slider_base.png"));
         unit = new Texture(Gdx.files.internal(images_path + "slider_unit.png"));
-
-        offset = unit.getWidth()/2;
-
+        // Disposition
         x = _x;      y = _y;
-        min = _min;  max = _max + 1;
+        min = _min;  max = _max;
+        offset = screenWidth / 20;
+        space = ((x - screenWidth / 4) * 2 - offset * 2) / (float)(max - min);
+        // Slider's subscription
         label = _label;
-
-        // Setup default value
-        ux = (int)MathUtils.lerp( x - base.getWidth()/2 + offset,
-                x + base.getWidth()/2 - offset, (float)defValue/max );
-        sx = ux;
-
         // Touch zone
-        rect = new Rectangle(x - base.getWidth()/2, y - base.getHeight()/2,
-                base.getWidth(), base.getHeight());
-
+        bounds = new Rectangle(x - screenWidth / 4 + offset, y - screenHeight / 20,
+                (x - screenWidth / 4) * 2 - offset * 2, screenHeight / 10);
+        // Set default value
+        ux = (int)MathUtils.lerp(bounds.x, bounds.x + screenWidth / 2, (float) (defValue - min) / (max - min) );
+        sx = ux;
+        // Fonts
         font = new BitmapFont();
         font.setColor(Color.WHITE);
     }
@@ -47,25 +47,23 @@ public class Slider {
         if (Gdx.input.isTouched()) {
             int ix = Gdx.input.getX();
             int iy = (Gdx.input.getY() - Gdx.graphics.getHeight()) * -1;
-
-            if (rect.contains(ix, iy)) {
+            // Slider will be affected only if touch is inside slider
+            if (bounds.contains(ix, iy))
                 ux = ix;
-                if (ux < x - base.getWidth()/2 + offset)
-                    ux = x - base.getWidth()/2 + offset;
-                if (ux > x + base.getWidth()/2 - offset)
-                    ux = x + base.getWidth()/2 - offset;
-            }
         }
+        // Received value
+        float slide = (ux - bounds.x) / bounds.width;
+        value = MathUtils.round((max - 1) * slide) + 1;
+        // Rigorous point
+        ux = (int)(bounds.x + ((value - 1) * space));
+        // Position of slider's pointer
         sx += (ux - sx) / 5;
-
-        float slide = (float) (ux - x + base.getWidth()/2 - offset) / (base.getWidth() - offset*2 + 1);
-        value = (int)MathUtils.lerp(min, max, slide);
     }
 
     public void draw(SpriteBatch batch){
-        batch.draw(base, x - base.getWidth()/2, y - base.getHeight()/2);
-        batch.draw(unit, sx - unit.getWidth()/2, y - unit.getHeight()/2);
-        font.draw(batch, label + Integer.toString(value), x - base.getWidth()/3,
-                y + (int)(base.getHeight()*0.7));
+        batch.draw(base, x - screenWidth / 4, y - screenHeight / 20, (x - screenWidth / 4) * 2, screenHeight / 10);
+        batch.draw(unit, sx - screenWidth / 20, y - screenHeight / 20, screenWidth / 10, screenHeight / 10);
+        font.draw(batch, label + Integer.toString(value), x - base.getWidth() / 3,
+                y + (int)(base.getHeight() * 0.7));
     }
 }
