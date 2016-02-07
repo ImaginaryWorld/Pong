dad28e372e8f97fb5e90b591ad426de3487e326d
SkyReMore <skyremore@mail.ru>
2016-02-07 11:24:09 +0300

    Android scroller bug fixed
    Class Slider
    
    ux, sx, left and right variables changed to float

--- core/src/com/phuda/pong/UI/Slider.java
+++ core/src/com/phuda/pong/UI/Slider.java
@@ -12,69 +12,61 @@ import com.badlogic.gdx.math.Rectangle;
 public class Slider {
 
     public int value;
-    private int ux, sx, x, y, min, max, screenWidth, screenHeight;
-    private float offset, space;
+    private int x, y, min, max;
+    private float ux, sx, offset, space;
     private String label;
     private Rectangle bounds;
     private Texture base, unit;
     private BitmapFont font;
-    private int left, right;
-    // Getting ratio of screen in desktop base (500 x 700)
-    private float hAspect = (Gdx.graphics.getWidth() + Gdx.graphics.getHeight())
-            / (float)(500 + 700);
+    private float left, right;
+    private float hAspect;
 
-    public Slider(int _x, int _y, int _min, int _max, int defValue, String _label){
-        screenWidth = Gdx.graphics.getWidth();
-        screenHeight = Gdx.graphics.getHeight();
+    public Slider(int _x, int _y, int _min, int _max, int defValue, String _label) {
         String images_path = "images_hi/";
         base = new Texture(Gdx.files.internal(images_path + "slider_base.png"));
         base.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
         unit = new Texture(Gdx.files.internal(images_path + "slider_unit.png"));
         unit.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-
+        // Getting ratio of screen in desktop base (500 x 700)
+        hAspect = (Gdx.graphics.getWidth() + Gdx.graphics.getHeight())
+                / (float)(500 + 700);
         x = _x;      y = _y;
         min = _min;  max = _max;
         label = _label;
-
         // Distance from start to active zone
-        offset = (int)(unit.getWidth()/2 * hAspect);
+        offset = unit.getWidth() / 2 * hAspect;
         // Bounds
-        left = (int)(x - (base.getWidth()/2 - offset) * hAspect);
-        right = (int)(x + (base.getWidth()/2 - offset) * hAspect);
-        // Setup default value
-        ux = (int)MathUtils.lerp(left, right, (float) (defValue - min) / (max - min) );
-        sx = ux;
-
+        left = (x - base.getWidth()/2 * hAspect + offset);
+        right = (x + base.getWidth()/2 * hAspect - offset);
         // Touch zone
-        bounds = new Rectangle(x - base.getWidth()/2 * hAspect + offset, y - base.getHeight()/2 * hAspect,
-                base.getWidth() * hAspect - offset * 2, base.getHeight() * hAspect);
+        bounds = new Rectangle(left, y - base.getHeight()/2 * hAspect,
+                right - left, base.getHeight() * hAspect);
+        // Setup default value
+        ux = MathUtils.lerp(left, right, (float) (defValue - min) / (max - min));
+        sx = ux;
+        // Distance between two points of slider
         space = (bounds.width / (float)(max - min));
-
+        // Fonts
         font = new BitmapFont();
         font.setColor(Color.WHITE);
     }
 
-    public void isPressed(){
+    public void isPressed() {
         if (Gdx.input.isTouched()) {
             int ix = Gdx.input.getX();
             int iy = (Gdx.input.getY() - Gdx.graphics.getHeight()) * -1;
-
+            // Press on slider
             if (bounds.contains(ix, iy))
                 ux = ix;
         }
-
+        // Number of received point on slider
+        int point = MathUtils.round((ux - left) / space);
         // Received value
-        float slide = (ux - bounds.x) / bounds.width;
-        value = MathUtils.round((max - 1) * slide) + 1;
+        value = point + min;
         // Rigorous point
-        ux = (int)(bounds.x + ((value - 1) * space));
-        // Position of slider's pointer
-        sx += (ux - sx) / 5;
-        /*
-        float slide = (ux - left - offset) / (base.getWidth() * hAspect - offset * 2 + 1);
-        value = (int)MathUtils.lerp(min, max, slide);
+        ux = left + (point * space);
         // Graphical slider slow follow
-        sx += (ux - sx) / 5;*/
+        sx += (ux - sx) / 5;
     }
 
     public void draw(SpriteBatch batch){
